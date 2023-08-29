1ff7b45c12c1ee900dd9291ebaef80e25fb399c7
artesah <sakhno@involve.software>
2023-08-29 19:45:28 +0300

    black

--- apps/bot/src/utils/operation.py
+++ apps/bot/src/utils/operation.py
@@ -2,7 +2,10 @@ from aiogram import Bot
 
 from apps.bot.config import CONTENT_FILENAME
 from apps.bot.src.content import load_content
-from apps.bot.src.keyboards.inline import operation_set_category_keyboard, operation_set_type_keyboard
+from apps.bot.src.keyboards.inline import (
+    operation_set_category_keyboard,
+    operation_set_type_keyboard,
+)
 from libs.constants import OperationSources, OperationTypes
 from libs.models import Operation, Chat
 
@@ -11,8 +14,13 @@ _content = load_content(CONTENT_FILENAME)
 
 async def send_operation_message(bot: Bot, operation: Operation):
     text = _content["messages"]["new_operation"].format(
-        type=_content["constants"]["OperationTypes"][OperationTypes(operation.type).name],
-        source=_content["constants"]["OperationSources"][OperationSources(operation.source).name] or "",
+        type=_content["constants"]["OperationTypes"][
+            OperationTypes(operation.type).name
+        ],
+        source=_content["constants"]["OperationSources"][
+            OperationSources(operation.source).name
+        ]
+        or "",
         amount=operation.amount,
         comment=operation.comment or "",
     )
@@ -22,11 +30,8 @@ async def send_operation_message(bot: Bot, operation: Operation):
     else:
         reply_markup = await operation_set_category_keyboard(
             operation.id,
-            operation.user.Service.get_active_operation_categories(operation.type))
+            operation.user.Service.get_active_operation_categories(operation.type),
+        )
 
     chat = Chat.select(Chat.id).where(Chat.user_id == operation.user_id).first()
-    await bot.send_message(
-        chat_id=chat.id,
-        text=text,
-        reply_markup=reply_markup
-    )
+    await bot.send_message(chat_id=chat.id, text=text, reply_markup=reply_markup)
